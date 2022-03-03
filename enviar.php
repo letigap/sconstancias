ec4f5746282a81190ec46b40ba68c7210c860a03
letigap <letiga@unam.mx>
2022-03-02 19:59:38 -0600

    envio de correo con rfc email y id de evento

--- enviar.php
+++ enviar.php
@@ -7,26 +7,15 @@ require 'PHPMailer/Exception.php';
 require 'PHPMailer/PHPMailer.php';
 require 'PHPMailer/SMTP.php';
 
-$mail = new PHPMailer(true);
-
 include_once("include/dbConexion.php");
 
-$array = $_GET['keys'];
-$correos = explode(",", $array);
-foreach ($emails as $indice => $valor){
-echo $indice." = ".$valor."<br>";
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-// $envio=$_POST['enviados'];
-//         print_r($envio);
+$idevento = $_GET['idevento']; 
+$id = $_GET['id'];
 
-// foreach($envio as $llave => $valor) {
-//     echo "el valor es: ".$valor. "<- ";  
-// $archivo = $_GET['id'];
+$correo = $_GET['correo'];
+$mail = new PHPMailer(true);
 
-// $correo = $_GET['correo'];
-    $correo="solrac512@gmail.com"
-
-    try {
+try {
     //Server settings
     $mail->SMTPDebug = 2;                                       // Enable verbose debug output
     $mail->isSMTP();                                            // Set mailer to use SMTP
@@ -45,18 +34,19 @@ echo $indice." = ".$valor."<br>";
 
     //Recipients
     $mail->setFrom('supervisor@www.depfe.unam.mx', 'Depfe');
-    $mail->addAddress($correo, 'destino');     // Add a recipient
+     $mail->addAddress($correo, 'destino');     // Add a recipient
+    // $mail->addAddress($row['EmailParticipante'], 'destino');     // Add a recipient
     $mail->addAddress('letiga@unam.mx', 'copia');     // Add a recipient
 
     // Attachments
-    $mail->AddAttachment('CONST_2021/Const-'.$valor.'.pdf');
+    // $mail->AddAttachment('CONST_2022/Const-'.$row['IdEvento'].''.$row['RfcParticipante'].'.pdf');
+    $mail->AddAttachment('CONST_2022/Const-'.$idevento.''.$id.'.pdf');
     //$mail->AddAttachment('enviar.php');
     // Content
     $mail->isHTML(true);                                  // Set email format to HTML
     $mail->Subject = 'Constancia de Evento';
     $mail->Body    = 'Para la Divisi&oacute;n de Estudios de Posgrado de la Facultad de Econom&iacute;a de la UNAM, es un gusto dirigirnos</br>
-a usted para agradecer su asistencia al Seminario en honor de Anthony P. Thirlwall: "Problem&aacute;ticas filos&oacute;ficas y anal&iacute;ticas en el desarrollo econ&oacute;mico de econom&iacute;as estructuralmente restringidas".
-//a usted para agradecer su asistencia al Seminario Internacional: "Econom&iacute;a en Tiempos de Pandemia." 
+a usted para agradecer su asistencia al III Seminario "Perspectivas críticas de las Cadenas Globales de Valor".
 </br>
 </br>
 Adjuntamos en este correo su constancia de asistencia y estaremos honrados de poder contar con su presencia en futuros eventos.</br>
@@ -72,10 +62,4 @@ La Divisi&oacute;n de Estudios de Posgrado de la Facultad de Econom&iacute;a-UNA
     echo "Hubo un error al enviar el mensaje. Mailer Error: {$mail->ErrorInfo}";
 }
 
-// }//termina foreach
-
-
-////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-}
-
-
+//  }//termina foreach
